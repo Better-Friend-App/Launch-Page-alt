48f2e47713aa870d5f84eb5acb1ef773eb3f1352
hschaen <hschaen@gmail.com>
2020-03-09 09:17:00 -0700

    Adding new alt Landing Page

---
+++ build/precache-manifest.7feeb82754837399a0255b5095b7acac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0991e87dbb9672c42ba68c5bee7a5f50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "208e1511078cb0665b1d",
+    "url": "/static/css/main.d24070c3.chunk.css"
+  },
+  {
+    "revision": "6f35eb0ba768ca988c5f",
+    "url": "/static/js/2.8aaff465.chunk.js"
+  },
+  {
+    "revision": "37b5fb9fa73e0688add42527cf5813eb",
+    "url": "/static/js/2.8aaff465.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "208e1511078cb0665b1d",
+    "url": "/static/js/main.0165b82c.chunk.js"
+  },
+  {
+    "revision": "364f20451ddf52087b40",
+    "url": "/static/js/runtime-main.7558c4d0.js"
+  }
+]);
